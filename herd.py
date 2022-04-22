db088abcaad89d6d625c6d4cbf3f88ed174799d8
Humm <jhumm@mcw.edu>
2022-04-22 16:25:11 -0500

    feat(herd.py): Created Herd Class

---
+++ herd.py
@@ -0,0 +1,22 @@
+class Herd():
+    def __init__(self) -> None:
+        self.dinosaurs = []
+        self.number_of_dinosaurs_alive = 0
+ 
+    def create_heard(self,dinosaur):
+        self.dinosaurs.append(dinosaur)
+        self.number_of_dinosaurs_alive += 1
+
+    def dinosaurss_left_alive(self):
+        alive_dinosaurs = []
+        for dinosaur in self.dinosaurs:
+            if dinosaur.is_alive:
+                alive_dinosaurs.append(dinosaur)
+        return alive_dinosaurs
+
+    def dinosaurs_that_can_attack_this_round(self,dinosaur_list):
+        dinosaur_attack_list = []
+        for dinosaur in dinosaur_list:
+            if dinosaur.can_attack_this_round:
+                dinosaur_attack_list.append(dinosaur)
+        return dinosaur_attack_list
